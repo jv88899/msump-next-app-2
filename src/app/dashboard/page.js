ce5f11b5cac47b6d0c973696d6cbe8670af9feb4
jv88899 <jv88899@gmail.com>
2023-08-01 12:14:29 -0500

    final commit before restart

--- src/app/dashboard/page.js
+++ src/app/dashboard/page.js
@@ -4,8 +4,126 @@ export default function Dashboard() {
   return (
     <div className={styles.wrapper}>
       <div className={styles["inner-wrapper"]}>
-        <h2>Dashboard</h2>
-        <div></div>
+        <div className={styles["headline-wrapper"]}>
+          <div className={styles.fixed}>
+            <h2>Dashboard</h2>
+          </div>
+        </div>
+        <div>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequuntur,
+          rem numquam dignissimos sed amet saepe delectus velit est tempore
+          maiores fugit, molestiae autem aut maxime deserunt quibusdam
+          distinctio atque ut facilis! At fuga eligendi, quis similique
+          corporis, molestiae suscipit veritatis temporibus dicta assumenda
+          nostrum eos repudiandae minus soluta odio magnam repellat corrupti
+          praesentium quos illo aspernatur. Accusantium mollitia repudiandae
+          adipisci expedita consectetur perferendis, quos incidunt nihil sint
+          ea, temporibus cumque vitae eius, exercitationem magnam distinctio
+          fugit quod natus explicabo in suscipit dolorem? Quos, suscipit quia
+          laborum dicta tenetur voluptas, rerum ipsum debitis impedit rem harum
+          minus dignissimos eum voluptatibus dolorem doloribus omnis officia
+          vitae fuga aspernatur necessitatibus, nam minima fugit! Vel quae harum
+          labore? Minus, quo? Cupiditate similique suscipit ab ipsam vel
+          officiis mollitia ducimus quibusdam incidunt libero sapiente nobis
+          harum vitae delectus cum impedit voluptatem enim nihil, quas itaque,
+          sequi tenetur fugiat possimus reprehenderit! Blanditiis laborum,
+          accusamus quia sit voluptatum fuga maiores assumenda eos quam
+          repudiandae, optio, vel tenetur deleniti nihil hic fugit perspiciatis
+          molestias iusto eum doloribus. Accusamus sed totam eum beatae
+          molestias quam facilis, eligendi architecto autem veniam labore
+          necessitatibus ipsam cum officia nobis? Possimus consequatur harum
+          reiciendis velit saepe, illo ratione atque tempora id quaerat
+          voluptatem iste soluta similique porro iure sequi non natus provident.
+          Voluptatem repellat ullam possimus, quae nostrum blanditiis doloribus,
+          fugiat sapiente et dolorem iure. Commodi corrupti omnis explicabo
+          accusamus ipsum dicta, obcaecati earum nulla minus non autem
+          asperiores, repellat eaque itaque, error quas assumenda quibusdam
+          blanditiis cum odit. Voluptates fugiat quaerat voluptatibus officia.
+          Quisquam odit necessitatibus consequuntur esse nostrum expedita hic
+          voluptate atque enim voluptates suscipit velit omnis, quas illo totam
+          modi quos ab repudiandae molestias quis nihil laboriosam officiis ex.
+          Enim recusandae, ut consequuntur beatae facilis animi sed maxime
+          minima maiores eligendi qui sapiente voluptas similique corporis.
+          Debitis culpa aut officiis placeat odit magnam similique adipisci
+          explicabo recusandae dolorum architecto ad saepe nemo, rerum, harum
+          illum. Voluptatem accusantium repellendus officia magni tempore,
+          aliquid enim eveniet laboriosam eius labore non molestiae sapiente,
+          omnis, neque quae. Tenetur ab sit neque quo recusandae fugit animi
+          eos, at voluptas quasi esse minus inventore. Aspernatur ab dolores
+          minus sunt excepturi doloremque quae animi autem pariatur voluptatibus
+          ratione accusantium iusto, dolore, tempore accusamus saepe vel id nisi
+          cumque. Rem consectetur repellendus quos quibusdam sit impedit
+          dignissimos cum architecto! Ullam enim magni reiciendis, eligendi
+          illum quam sunt soluta perspiciatis minima dolore quaerat ipsam omnis
+          doloremque sint. Delectus animi assumenda, eius minus nulla eveniet
+          eligendi, ipsum aspernatur ullam modi necessitatibus facilis aperiam
+          laudantium, qui maiores beatae ea officiis! Cum fuga fugiat
+          repudiandae architecto mollitia nostrum reprehenderit, porro rem
+          assumenda! Dolor aspernatur perspiciatis officia excepturi, soluta
+          nemo deleniti iure blanditiis possimus. Commodi maxime, deleniti iste
+          voluptates totam numquam quod laborum illum molestiae aliquid delectus
+          aut cum nihil quo autem nemo. Repellat officia cupiditate tempora
+          harum quos inventore nam. Omnis rem sint aliquid incidunt optio
+          voluptate iusto aut saepe repellat sequi. Illo nisi tenetur voluptatum
+          ut minus ratione mollitia, totam aliquam quod dolorum! Ex eaque iure
+          quis aliquid molestiae tempore eveniet perspiciatis consequuntur
+          delectus, deleniti error esse recusandae voluptate earum reiciendis
+          sit hic doloribus fugit officia vero nam? Neque, sequi ab! Iure labore
+          nesciunt et atque omnis temporibus dolorem unde aliquam. Architecto
+          laudantium mollitia aperiam minima, repudiandae totam commodi
+          perspiciatis quas hic? Voluptate et a magni perferendis corporis
+          ducimus quasi eos, tempora ex optio maxime, qui earum nesciunt
+          molestias? Deserunt sapiente ullam molestias aut hic consequuntur
+          voluptas, veritatis reprehenderit non! Odio atque assumenda commodi
+          doloremque. Nulla, pariatur facere. Quidem nemo similique eius placeat
+          ea ab quam, assumenda repellat odit neque sed natus labore, nam
+          eligendi soluta, recusandae officiis. Ullam sed perferendis earum
+          iusto ipsum laboriosam accusamus distinctio, vitae recusandae fugit
+          deleniti, impedit ex sit cum quia eaque alias? Voluptas cumque,
+          labore, expedita reprehenderit libero laudantium perferendis
+          temporibus maiores modi illo eius. Quos cupiditate ratione odio iusto
+          expedita nobis officiis, maiores tempora eius at natus ipsam minus
+          dolorum deserunt architecto? Iusto eveniet repudiandae at, consequatur
+          alias nemo veritatis minus nam molestias, praesentium ratione,
+          architecto error facilis corrupti? Atque cupiditate iusto iste labore
+          necessitatibus, odio perferendis voluptates ullam voluptatem at illo
+          deserunt praesentium ratione rerum libero quibusdam sit eius quidem ad
+          modi earum. Pariatur quo nisi cum eius, nam repudiandae? Facere sint
+          saepe fugiat mollitia ab corrupti vero dolore voluptatem modi culpa!
+          Maiores labore a quam pariatur amet saepe quod nisi nihil ex iusto
+          quibusdam, fuga esse eligendi commodi officia et. Iure aperiam illum
+          id, possimus eius nobis nemo quas facilis voluptates vel excepturi
+          veritatis omnis recusandae aliquid fugit? Numquam illo tempora
+          necessitatibus suscipit ea aperiam molestias enim aliquid, voluptate
+          qui veniam quisquam cumque, delectus recusandae molestiae, mollitia
+          temporibus. Ratione cumque, consequatur illum consequuntur illo
+          assumenda similique odit nihil temporibus sed ab vitae, nesciunt
+          facilis laborum eveniet animi molestias ullam deleniti necessitatibus.
+          Esse ut libero voluptates nisi maxime, assumenda laborum repellat
+          architecto doloribus? Magnam, possimus eius. A et porro sed alias,
+          suscipit hic eos architecto eum laborum accusamus veniam quod unde
+          cupiditate aspernatur ipsum dolorum debitis animi minima, ad amet
+          dicta voluptatem quibusdam? Iste totam voluptatem obcaecati mollitia
+          aperiam asperiores nihil adipisci ad autem sed harum repudiandae culpa
+          aspernatur nisi debitis, repellendus error aut doloremque cupiditate
+          labore consequuntur odit. Modi laborum quae saepe dolore libero maxime
+          exercitationem! Eum, iure. Natus dolor tempore aspernatur obcaecati
+          maiores quo voluptate dicta odio totam voluptates! Nam, ipsa
+          repudiandae at deserunt totam necessitatibus ratione praesentium?
+          Nostrum, voluptas eveniet repudiandae dolorem numquam nesciunt iusto
+          veniam obcaecati! Architecto unde neque asperiores adipisci dolores
+          consequuntur sunt. Atque veritatis alias repellat doloribus porro
+          laudantium placeat tempora consectetur voluptatum omnis dignissimos
+          mollitia nam sunt fugit, dolor vitae quisquam nihil officia quis ad
+          aspernatur fuga ipsam. Corporis quidem maxime aut temporibus, hic
+          voluptates quas perspiciatis qui ratione quam eos explicabo a ea
+          architecto similique totam illo? Quisquam id, tempora obcaecati quam
+          praesentium doloribus quae temporibus repellat fuga. Ut facere quas
+          velit perferendis. Aperiam harum asperiores expedita, vitae fuga ad ex
+          mollitia rem aliquam dolores natus. Omnis in eligendi magnam vitae
+          quae asperiores voluptatem minus nulla cum quod! Repudiandae eligendi
+          ipsa, asperiores ex odio sunt.
+        </div>
       </div>
     </div>
   );
--- src/app/layout.js
+++ src/app/layout.js
@@ -1,6 +1,8 @@
 import NavigationMenu from "@/components/NavigationMenu/NavigationMenu";
 import "./globals.css";
 import { Inter } from "next/font/google";
+import styles from "./layout.module.css";
+import Community from "@/components/Community/Community";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -9,12 +11,36 @@ export const metadata = {
   description: "Join us as we search for the best albums ever created",
 };
 
+// export default function RootLayout({ children }) {
+//   return (
+//     <html lang="en">
+//       <body className={inter.className}>
+//         <div className={styles.wrapper}>
+//           <div className={styles["inner-wrapper"]}>
+//             <div style={{ width: "30%", backgroundColor: "blue" }}>
+//               <NavigationMenu />
+//             </div>
+//             <div style={{ width: "40%" }}>{children}</div>
+//             <div
+//               style={{
+//                 width: "30%",
+//                 backgroundColor: "green",
+//               }}
+//             >
+//               <Community />
+//             </div>
+//           </div>
+//         </div>
+//       </body>
+//     </html>
+//   );
+// }
+
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body className={inter.className}>
-        {children}
-        <NavigationMenu />
+        <div className=""></div>
       </body>
     </html>
   );
